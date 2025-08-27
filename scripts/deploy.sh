1d2ac684ef1e051aec2497dfdcb74ec9f7d40636
Bharat Kathi <bharat1031@gmail.com>
2025-08-27 17:10:52 -0400

    update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -17,3 +17,19 @@ fi
 echo "Building container for $service_name v$VERSION"
 # Build the docker container
 docker build -t gauchoracing/$service_name:"$VERSION" -t gauchoracing/$service_name:latest --platform linux/amd64,linux/arm64 --push --progress=plain .
+
+# Check if GitHub CLI is installed
+if ! command -v gh &> /dev/null
+then
+    echo "GitHub CLI (gh) is not installed. Please install it to proceed."
+    exit 1
+fi
+
+# Create a release tag
+git tag -s v$VERSION -m "Release version $VERSION"
+git push origin v$VERSION
+
+# Create a release
+gh release create v$VERSION --generate-notes
+
+echo "Package released successfully for version $VERSION"
